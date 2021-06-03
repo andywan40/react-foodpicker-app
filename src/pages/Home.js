8064154ce377ba5b87fccdd97298351e9aeb946e
andywan40 <andywan40@gmail.com>
2021-06-03 13:40:27 +0800

    use unsplash api to fetch img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 // import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 import { withStyles } from "@material-ui/styles";
-import RestaurantIcon from '@material-ui/icons/Restaurant';
+import RestaurantIcon from "@material-ui/icons/Restaurant";
 import styles from "../styles/HomeStyles";
 import foods from "../helperFunctions/foods";
 import { getRandomItem } from "../helperFunctions/random";
 import background from "../images/homebg.jpg";
 
+//TODO change text color depending on background darkness
 function Home(props) {
   const { classes } = props;
   const [cuisine, setCuisine] = useState("");
   const [dish, setDish] = useState("");
   const [bgImg, setBgImg] = useState(background);
+  const key = "v0QNKQFmkR9rzeGbt9wHp6CL1Ow4cJLBlCJOHS1X9vE";
+  const baseUrl = "https://api.unsplash.com";
   const handleClick = () => {
     //call api
     const item = getRandomItem(foods);
@@ -21,32 +24,42 @@ function Home(props) {
     const dish = getRandomItem(dishes);
     setCuisine(cuisine);
     setDish(dish);
-    getPhoto();
+    getPhoto(dish);
   };
-  const handleSearch = item => {
+  const handleSearch = (item) => {
     window.open(`https://www.google.com/search?q=${item}+near+me`);
-  }
-  const getPhoto = async () => {
-    const url = `https://api.pexels.com/v1/search?query=burger`;
-    const headers = {
-        Authorization: "563492ad6f91700001000001c6185b83741d425ca01d38c2a33e143f"
+  };
+  const getPhoto = async (dish) => {
+    let url = `${baseUrl}/search/photos?query=${dish}&client_id=${key}`;
+    let data = await axios.get(url);
+    let photos = data.data.results;
+    let photo;
+    if (photos.length !== 0){
+        photo = getRandomItem(photos).urls.regular;
+    }else{
+        url = `${baseUrl}/search/photos?query=${cuisine}&client_id=${key}`;
+        data = await axios.get(url);
+        photos = data.data.results;
+        photo = getRandomItem(photos).urls.regular;
     }
-    const data = await axios.get(url, {headers});
-    const photos = data.data.photos;
-    const photo = getRandomItem(photos).url;
+    console.log(photos)
     setBgImg(photo);
-    
-  }
+  };
   return (
     <div className={classes.Home} style={{backgroundImage: `url(${bgImg})`}}>
       <div>
         <h1 className={classes.title}>What To Eat?</h1>
-        <RestaurantIcon className={classes.restaurantIcon} onClick={handleClick}/>
+        <RestaurantIcon
+          className={classes.restaurantIcon}
+          onClick={handleClick}
+        />
       </div>
       <div className={classes.result}>
-        <h3 onClick={ () => handleSearch(cuisine)}>{cuisine}</h3>
-        <h4 onClick={ () => handleSearch(dish)}>{dish}</h4>
+        <h3 onClick={() => handleSearch(cuisine)}>{cuisine}</h3>
+        <h4 onClick={() => handleSearch(dish)}>{dish}</h4>
       </div>
+      {/* <img src={bgImg} alt="pic" width="100" height="100" /> */}
+      
     </div>
   );
 }
