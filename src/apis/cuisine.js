e8e46b333298e1e379b7e2882e67d29075fa098f
andywan40 <andywan40@gmail.com>
2021-06-05 20:44:39 +0800

    update api urls

--- src/apis/cuisine.js
+++ src/apis/cuisine.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getCuisinePhotos = async (cuisine, currentPage) => {
-  const url = `https://react-foodpicker-app-backend.com/api/v1/unsplash/cuisine/${cuisine}/${currentPage}`;
+  const url = `https://react-foodpicker-app-backend.herokuapp.com/api/v1/unsplash/cuisine/${cuisine}/${currentPage}`;
   try {
     const data = await axios.get(url);
     return data.data;
