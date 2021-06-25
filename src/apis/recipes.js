e678c22d5ce7bb7511ac598d414bee695bf96597
andywan40 <andywan40@gmail.com>
2021-06-25 16:16:05 +0800

    update edamam api

--- src/apis/recipes.js
+++ src/apis/recipes.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 
 export const getCuisineRecipes = async (cuisine, nextLink) => {
-    const url = nextLink === "" ? `http://localhost:4545/api/v1/edamam/cuisine/${cuisine}/null` :  `http://localhost:4545/api/v1/edamam/cuisine/${cuisine}/${nextLink}`;   
-//   const url = `https://react-foodpicker-app-backend.herokuapp.com/api/v1/edamam/cuisine/${cuisine}`;
+  const url = nextLink === "" ? `https://react-foodpicker-app-backend.herokuapp.com/api/v1/edamam/cuisine/${cuisine}/null` :  `https://react-foodpicker-app-backend.herokuapp.com/api/v1/edamam/cuisine/${cuisine}/${nextLink}`;   
   try {
     const data = await axios.get(url);
     return data.data;
   } catch (e) {
+    console.log(e)
     throw new Error(e);
   }
 };
 
 export const getDishRecipes = async (dish, nextLink) => {
-    const url = nextLink === "" ? `http://localhost:4545/api/v1/edamam/dish/${dish}/null` :  `http://localhost:4545/api/v1/edamam/dish/${dish}/${nextLink}`;   
-//   const url = `https://react-foodpicker-app-backend.herokuapp.com/api/v1/edamam/cuisine/${cuisine}`;
+  const url = nextLink === "" ? `https://react-foodpicker-app-backend.herokuapp.com/api/v1/edamam/dish/${dish}/null` :  `https://react-foodpicker-app-backend.herokuapp.com/api/v1/edamam/dish/${dish}/${nextLink}`;
   try {
     const data = await axios.get(url);
     return data.data;
